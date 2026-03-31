120ea01733fae316094b1a8798f924c9afaa40f6
codeTukai <tukaighosh753@gmail.com>
2026-03-31 19:59:04 +0530

    changes

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,27 @@ import Footer from "./Footer/Footer";
 import Container from './container/Container'
 import Logo from '../Logo'
 import LogoutBtn from './Header/LogoutBtn'
+import RTE from './RTE'
+import Signup from "../pages/Signup";
+import PostForm from "./post-form/PostForm";
+import Login from "../pages/Login";
+
+
+import PostCard from './PostCard'
+import AuthLayout from './AuthLayout'
+
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+   AuthLayout,
+    PostCard
+    
 }
